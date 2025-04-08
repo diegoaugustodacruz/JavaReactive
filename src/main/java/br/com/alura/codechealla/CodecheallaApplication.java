d8bbfbd40b1559eebd7448816444fb9c301b4acc
diego-cruz <diegoaugustodacruz@gmail.com>
2025-04-08 11:28:33 -0300

    inicio projeto

---
+++ src/main/java/br/com/alura/codechealla/CodecheallaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.codechealla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecheallaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecheallaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/codechealla/CodecheallaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.codechealla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecheallaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
